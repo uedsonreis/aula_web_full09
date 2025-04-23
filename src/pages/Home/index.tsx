46b5e634e27565f94747566827f0c092603ece5f
Uedson Reis <uedson.reis@sovos.com>
2025-04-23 20:10:04 -0300

    WIP

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,61 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import * as userService from '../../services/user.service'
+
+import './styles.scss'
 
 export default function HomePage() {
+    const navigate = useNavigate()
 
-    let userList = []
+    const [userList, setUserList] = React.useState([])
+
+    function fetchUsers() {
+        userService.getList().then(list => {
+            setUserList(list)
+        }).catch(error => {
+            console.error('Erro ao recuperar a lista de usuários: ', error)
+            navigate('/login')
+        })
+    }
+
+    React.useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    function goToAdd() {
+        navigate('/users/create')
+    }
+
+    function remove(id: number) {
+        userService.remove(id).then(() => {
+            fetchUsers()
+        }).catch(error => {
+            console.error('Erro ao deletar um usuário: ', error)
+            navigate('/login')
+        })
+    }
 
     return (
-        <div>
-            <header>Lista de Usuários</header>
+        <div className='home-page'>
+            <header>
+                <div></div>
+                <h2>Lista de Usuários</h2>
+                <button onClick={goToAdd}>Add</button>
+            </header>
 
             <main>
-
+                {
+                    userList.map((user: any) => {
+                        return (
+                            <div className='list-item'>
+                                <div className='title'>{user.name}</div>
+                                <div>{user.id}</div>
+                                <div className='sub-title'>{user.username}</div>
+                                <button onClick={() => remove(user.id)}>Remover</button>
+                            </div>
+                        )
+                    })
+                }
             </main>
 
             <footer>
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,7 +3,6 @@ import { getToken } from './auth.service'
 const url = 'http://localhost:3030/users'
 
 export async function save(name: string, username: string, password: string) {
-
     const token = getToken()
 
     const response = await fetch(url, {
@@ -16,5 +15,37 @@ export async function save(name: string, username: string, password: string) {
     })
 
     return response.status
+}
+
+export async function getList() {
+    const token = getToken()
+
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+    })
+
+    if (response.status !== 200) throw new Error()
+
+    return await response.json()
+}
+
+export async function remove(id: number) {
+    const token = getToken()
+
+    const response = await fetch(url +"/"+ id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+    })
+
+    if (response.status !== 200) throw new Error()
+
+    return await response.json()
 }
 
